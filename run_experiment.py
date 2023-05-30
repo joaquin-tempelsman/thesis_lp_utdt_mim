0a8f7c6545d5b2a5a40a65eb38ce72e74fcecedb
joaquin tempelsman <jtempelsman@gmail.com>
2023-05-30 14:37:47 -0300

    run all exp

--- run_experiment.py
+++ run_experiment.py
@@ -30,7 +30,7 @@ log.addHandler(ch)
 
 delete_log_files("lp_logs")
 exp_grid = {
-    # variable prices - end of the world extra slack
+    # # variable prices - end of the world extra slack
     '2019_24periods_12eow_real_prices' : {'fecha_inicio': '18/01/2019', 'periodos_modelo': 36, 'eow': 24 , 'fecha_fin_ejercicio': '08/01/2021', 'fix_prices': False, "mantain_c3_stock": 0},
     'm2019_24periods_12eow_real_prices' : {'fecha_inicio': '07/06/2019', 'periodos_modelo': 36, 'eow': 24  , 'fecha_fin_ejercicio': '03/06/2021', 'fix_prices': False, "mantain_c3_stock": 0},
     '2020_24periods_12eow_real_prices' : {'fecha_inicio': '03/01/2020', 'periodos_modelo': 36, 'eow': 24 , 'fecha_fin_ejercicio': '06/01/2022', 'fix_prices': False, "mantain_c3_stock": 0},
@@ -46,7 +46,7 @@ exp_grid = {
     '2019_42period_real_prices' : {'fecha_inicio': '18/01/2019', 'periodos_modelo': 42, 'fecha_fin_ejercicio': '02/06/2022', 'fix_prices': False, "mantain_c3_stock": 1},
     '2019_48period_real_prices' : {'fecha_inicio': '18/01/2019', 'periodos_modelo': 48, 'fecha_fin_ejercicio': '05/01/2023', 'fix_prices': False, "mantain_c3_stock": 1},
 
-    # fix prices (non comparable to business)  - restriction to mantain c3 stock from period 0 on end
+    # # fix prices (non comparable to business)  - restriction to mantain c3 stock from period 0 on end
     '2019_24periods_fix_prices' : {'fecha_inicio': '18/01/2019', 'periodos_modelo': 24, 'fecha_fin_ejercicio': '08/01/2021', 'fix_prices': True, "mantain_c3_stock": 1},
     'm2019_24periods_fix_prices' : {'fecha_inicio': '07/06/2019', 'periodos_modelo': 24, 'fecha_fin_ejercicio': '03/06/2021', 'fix_prices': True, "mantain_c3_stock": 1},
     '2020_24periods_fix_prices' : {'fecha_inicio': '03/01/2020', 'periodos_modelo': 24, 'fecha_fin_ejercicio': '06/01/2022', 'fix_prices': True, "mantain_c3_stock": 1},
@@ -64,7 +64,10 @@ for experiment, items in exp_grid.items():
     if PARAMS["mantain_c3_stock"] == 1:
         pass
 
-    initial_stock_cost, df_precios = build_LP_inputs(
+    (
+        exp_grid[experiment]["lp_stock_history_cost"],
+        df_precios,
+    ) = build_LP_inputs(
         PARAMS,
         PATH_DAT_FILES,
         path_scrapped_prices_df,
@@ -75,7 +78,6 @@ for experiment, items in exp_grid.items():
         INITIAL_STOCK_TEST=False,
         fix_prices=items["fix_prices"],
     )
-    exp_grid[experiment]["lp_stock_history_cost"] = initial_stock_cost
 
     #run LP
     os.system(
@@ -83,10 +85,11 @@ for experiment, items in exp_grid.items():
     )
 
     # compare with business variant data
-    business_excercise = business_variant(
+    business_excercise, df_ventas, df_final_stock_value = business_variant(
         PARAMS, PESOS_PROMEDIO, path_parte_diario, path_scrapped_prices_df
     )
     exp_grid[experiment]["business_results"] = business_excercise
+    df_final_stock_value.to_csv(f"lp_logs/df_final_stock_value_{experiment}.csv")
 
     # ! required by explore_results.ipynb
     pfix = items['fix_prices']
